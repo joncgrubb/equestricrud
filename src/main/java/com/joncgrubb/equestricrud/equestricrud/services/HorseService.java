f82944b365c616e551f17ceaec08b9d61a5a5b27
joncgrubb <joncgrubbdev@gmail.com>
2021-07-27 11:59:49 -0400

    Completed data manipulation for views in all service methods except delete horse. Made changes to Thymeleaf data references in both templates to correctly show and input correct data.

--- src/main/java/com/joncgrubb/equestricrud/equestricrud/services/HorseService.java
+++ src/main/java/com/joncgrubb/equestricrud/equestricrud/services/HorseService.java
@@ -3,11 +3,8 @@ package com.joncgrubb.equestricrud.equestricrud.services;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
-// import java.util.ArrayList;
-// import java.util.List;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.joncgrubb.equestricrud.equestricrud.data.common.Gender;
 import com.joncgrubb.equestricrud.equestricrud.data.entities.Horse;
@@ -31,9 +28,6 @@ public class HorseService {
 	@RequestMapping("/horse/{id}")
 	public String horse(@PathVariable Long id, Model model) {
 		try {
-			System.out.println("*** Entering horse method ***");
-
-			// *** Start New Code ***
 			System.out.println("*** Finding new horse object ***");
 			Horse horse = horseRepository.findById(id).orElse(null);
 			HorseInterface horseInterface = new HorseInterface();
@@ -42,22 +36,14 @@ public class HorseService {
 				System.out.println("*** Horse not null ***");
 				horseInterface.setId(horse.getId());
 				horseInterface.setName(horse.getName());
-
-				// Calculate age of the horse based on currently held thoroughbred age
-				// conventions; Birth date will always be year of birth on January 1
-				LocalDate now = LocalDate.now();
-				int currentYear = now.getYear();
-				LocalDate foalDate = LocalDate.of(horse.getFoalYear(), Month.JANUARY, 1);
-				LocalDate currentDate = LocalDate.of(currentYear, Month.JANUARY, 1);
-				int age = Period.between(foalDate, currentDate).getYears();
-				String ageString = String.valueOf(age);
-
-				horseInterface.setAge(ageString);
-
-				// TODO: Do math for gender strings
-				
-				horseInterface.setGenderName("Filly");
+				horseInterface.setEquibaselink(horse.getEquibaselink());
+				horseInterface.setOwner(horse.getOwner());
+				horseInterface.setTrainer(horse.getTrainer());
+				horseInterface.setJockey(horse.getJockey());
+				horseInterface.setGenderName(String.valueOf(horse.getGender()));
+				horseInterface.setAge(String.valueOf(horse.getFoalYear()));
 			}
+
 			model.addAttribute("horse", horseInterface);
 			return "horse";
 		} catch (Exception e) {
@@ -68,8 +54,49 @@ public class HorseService {
 	@RequestMapping(value = "/horses", method = RequestMethod.GET)
 	public String horseList(Model model) {
 		try {
-			model.addAttribute("horses", horseRepository.findAll());
+			List<HorseInterface> horseInterfaceList = new ArrayList<HorseInterface>();
+			List<Horse> horseList = horseRepository.findAll();
 
+			for (Horse horse : horseList) {
+				HorseInterface horseInterface = new HorseInterface();
+				if (horse != null) {
+					horseInterface.setId(horse.getId());
+					horseInterface.setName(horse.getName());
+					horseInterface.setEquibaselink(horse.getEquibaselink());
+					horseInterface.setOwner(horse.getOwner());
+					horseInterface.setTrainer(horse.getTrainer());
+					horseInterface.setJockey(horse.getJockey());
+
+					// Calculate age of the horse based on currently held thoroughbred age
+					// conventions; Birth date will always be year of birth on January 1
+					LocalDate now = LocalDate.now();
+					int currentYear = now.getYear();
+					LocalDate foalDate = LocalDate.of(horse.getFoalYear(), Month.JANUARY, 1);
+					LocalDate currentDate = LocalDate.of(currentYear, Month.JANUARY, 1);
+					int age = Period.between(foalDate, currentDate).getYears();
+					String ageString = String.valueOf(age);
+
+					horseInterface.setAge(ageString);
+
+					// Assign displayed horse gender based on age and actual gender
+					String horseGender = null;
+					if (horse.getGender().equals(Gender.f) && age < 4) {
+						horseGender = "Filly";
+					} else if (horse.getGender().equals(Gender.f)) {
+						horseGender = "Mare";
+					} else if (horse.getGender().equals(Gender.m) && age < 4) {
+						horseGender = "Colt";
+					} else if (horse.getGender().equals(Gender.m)) {
+						horseGender = "Horse";
+					}
+
+					horseInterface.setGenderName(horseGender);
+
+					horseInterfaceList.add(horseInterface);
+				}
+			}
+
+			model.addAttribute("horses", horseInterfaceList);
 			return "horses";
 		} catch (Exception e) {
 			throw e;
@@ -77,7 +104,7 @@ public class HorseService {
 	}
 
 	@RequestMapping(value = "/horses", method = RequestMethod.POST)
-	public String horsesAdd(@RequestParam String name, @RequestParam Gender gender, @RequestParam String foalYear,
+	public String horsesAdd(@RequestParam String name, @RequestParam Gender gender, @RequestParam int foalYear,
 			@RequestParam String equibaselink, @RequestParam String owner, @RequestParam String trainer,
 			@RequestParam String jockey, Model model) {
 		try {
@@ -86,7 +113,7 @@ public class HorseService {
 			Horse newHorse = new Horse();
 			newHorse.setName(name);
 			newHorse.setGender(gender);
-			newHorse.setFoalYear(localDate);
+			newHorse.setFoalYear(foalYear);
 			newHorse.setEquibaselink(equibaselink);
 			newHorse.setOwner(owner);
 			newHorse.setTrainer(trainer);
@@ -103,8 +130,8 @@ public class HorseService {
 	}
 
 	@RequestMapping(value = "/horse/{id}", method = RequestMethod.POST)
-	public String horseEdit(@RequestParam long id, @RequestParam String name, @RequestParam Gender gender,
-			@RequestParam LocalDate foalYear, @RequestParam String equibaselink, @RequestParam String owner,
+	public String horseEdit(@RequestParam long id, @RequestParam String name, @RequestParam Gender genderName,
+			@RequestParam int age, @RequestParam String equibaselink, @RequestParam String owner,
 			@RequestParam String trainer, @RequestParam String jockey, Model model) {
 		try {
 			// TODO Change foalYear from LocalDate to Integer
@@ -112,8 +139,8 @@ public class HorseService {
 			System.out.println("************ Horse ID: " + id);
 			Horse horse = horseRepository.findById(id).get();
 			horse.setName(name);
-			horse.setGender(gender);
-			horse.setFoalYear(foalYear);
+			horse.setGender(genderName);
+			horse.setFoalYear(age);
 			horse.setEquibaselink(equibaselink);
 			horse.setOwner(owner);
 			horse.setTrainer(trainer);
